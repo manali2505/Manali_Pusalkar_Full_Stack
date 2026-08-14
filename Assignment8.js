1bdf5b9b0c6d32094a33b6edad4926f9b87ef9dd
manali2505 <manalip2505@gmail.com>
2026-08-14 12:59:15 +0530

    Delete Assignment8.js

--- Assignment8.js
+++
@@ -1,6 +0,0 @@
-const prompt=require(`prompt-sync`)();
-const number=parseInt(prompt("Enter the number from 1 to 10 "));
-for(let i=1;i<=10;i++)
-{
-    console.log(`${number} X ${i} = ${number *i}`);
-}
