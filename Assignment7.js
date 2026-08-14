36e83b56db9741299ca676ebf2732c44c1186a53
manali2505 <manalip2505@gmail.com>
2026-08-14 12:49:05 +0530

    Delete Assignment7.js

--- Assignment7.js
+++
@@ -1,9 +0,0 @@
-const prompt=require(`prompt-sync`)();
-const year=prompt("Enter the year to be checked ");
-if((((year % 4)==0)&&((year % 100)!=0))||((year%400)==0))
-{
-console.log(`The year ${year} is leap year `);
-}
-else{
-    console.log(`The year ${year} is not leap year`);
-}
